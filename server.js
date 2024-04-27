83c382d59b34fee7c1e28d8df364840069b98f64
Nahid Ahmed <npahmed92@gmail.com>
2024-04-27 16:05:37 -0400

    add middleware

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3001;
 const app = express();
 
 // MIDDLEWARE ========================================
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ROUTE =============================================
 

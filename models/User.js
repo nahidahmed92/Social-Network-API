33d0012ac926d133f050f670a756554b6d718261
Nahid Ahmed <npahmed92@gmail.com>
2024-04-28 11:10:04 -0400

    create virtual friendCount

--- models/User.js
+++ models/User.js
@@ -1,30 +1,42 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    trim: true,
-    unique: true,
-    required: true,
-  },
-  email: {
-    type: String,
-    match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
-    unique: true,
-    required: true,
-  },
-  thoughts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Thoughts',
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
     },
-  ],
-  friends: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+    email: {
+      type: String,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+      unique: true,
+      required: true,
     },
-  ],
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thoughts',
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
 });
 
 const User = mongoose.model('user', userSchema);

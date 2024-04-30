9bdad05f0c7879e541df50ca2e2e08558c2ce55e
Nahid Ahmed <npahmed92@gmail.com>
2024-04-29 22:10:12 -0400

    add getOneUser, createUser, updateUser controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,44 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  getSingleUser: async (req, res) => {
-    const user = await User.findOne({ _id: req.params.id });
+  getOneUser: async (req, res) => {
+    try {
+      const user = await User.findOne({ _id: req.params.id })
+        .populate('thoughts')
+        .populate('friends');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  createUser: async (req, res) => {
+    try {
+      const newUser = await User.create(req.body);
+
+      res.json(newUser);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+      res.json(updatedUser);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   },
 };

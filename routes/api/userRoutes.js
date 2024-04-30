269c9fe1bcfdecaf82e346be01fcd680b3491f53
Nahid Ahmed <npahmed92@gmail.com>
2024-04-29 22:10:25 -0400

    implement routes with controllers

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
+const {
+  getAllUsers,
+  createUser,
+  getOneUser,
+  updateUser,
+} = require('../../controllers/userController.js');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:id').get(getOneUser).put(updateUser);
 
 module.exports = router;

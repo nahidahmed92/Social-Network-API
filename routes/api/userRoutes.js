b29772e6b545cb6aef6fb9bed2cd5f817889e5d6
Nahid Ahmed <npahmed92@gmail.com>
2024-04-30 20:07:44 -0400

    set up routes for findAllFriends and addFriend

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,10 +5,14 @@ const {
   getOneUser,
   updateUser,
   deleteUser,
+  findAllFriends,
+  addFriend,
 } = require('../../controllers/userController.js');
 
 router.route('/').get(getAllUsers).post(createUser);
 
 router.route('/:id').get(getOneUser).put(updateUser).delete(deleteUser);
+
+router.route('/:id/friends').get(findAllFriends).post(addFriend);
 
 module.exports = router;

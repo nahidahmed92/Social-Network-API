3291c7a0db0f0691e82b0c8cf979a6b04c1a0644
Nahid Ahmed <npahmed92@gmail.com>
2024-05-01 21:20:22 -0400

    change seed to insertMany

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,6 +5,7 @@ const { userData, thoughtData } = require('./data.js');
 connection.once('open', async () => {
   console.log('connection opened');
   const userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+
   if (userCheck.length) {
     await connection.dropCollection('users');
     console.log('user database dropped');
@@ -15,21 +16,9 @@ connection.once('open', async () => {
     await connection.dropCollection('thoughts');
     console.log('thought database dropped');
   }
-  const users = await User.create(userData);
-  const userId = JSON.stringify(...users.map(({ _id }) => _id)).replace(/^"|"$/g, '');
 
-  for (const thought of thoughtData) {
-    const thoughts = await Thought.create({
-      thoughtText: thought.thoughtText,
-      username: thought.username,
-      reactions: [
-        {
-          reactionBody: thought.reactions[0].reactionBody,
-          username: thought.reactions[0].username,
-        },
-      ],
-    });
-  }
+  const users = await User.insertMany(userData);
+  const newThoughts = await Thought.insertMany(thoughtData);
 
   console.log('Database seeded');
   process.exit(0);

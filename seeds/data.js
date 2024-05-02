07edf49af279ca2cf4870ed039388f3a4c48e4ef
Nahid Ahmed <npahmed92@gmail.com>
2024-05-01 21:19:18 -0400

    add ids to users, thoughts and friends

--- seeds/data.js
+++ seeds/data.js
@@ -1,20 +1,29 @@
 const userData = [
   {
+    _id: '6631b6cefbd7ea0a740f8d8b',
     username: 'hookah101',
     email: 'hookah101@gmail.com',
+    thoughts: ['6631b71c9e2da83f2938057a'],
+    friends: ['6631b6cefbd7ea0a740f8d8c', '6631b6cefbd7ea0a740f8d8d'],
   },
   {
+    _id: '6631b6cefbd7ea0a740f8d8c',
     username: 'brittanyt',
     email: 'brittanythomas@gmail.com',
+    friends: ['6631b6cefbd7ea0a740f8d8b', '6631b6cefbd7ea0a740f8d8d'],
   },
   {
+    _id: '6631b6cefbd7ea0a740f8d8d',
     username: 'fullstackdev',
     email: 'fsd@gmail.com',
+    thoughts: ['6631b71c9e2da83f2938057e'],
+    friends: ['6631b6cefbd7ea0a740f8d8b', '6631b6cefbd7ea0a740f8d8c'],
   },
 ];
 
 const thoughtData = [
   {
+    _id: '6631b71c9e2da83f2938057a',
     thoughtText: 'Coming up with a video for a great way to pack a bowl.',
     username: 'hookah101',
     reactions: [
@@ -25,6 +34,7 @@ const thoughtData = [
     ],
   },
   {
+    _id: '6631b71c9e2da83f2938057e',
     thoughtText: 'Here is an amazing course to sign up for by Columbia Engineering Boot Camp.',
     username: 'fullstackdev',
     reactions: [
@@ -36,7 +46,5 @@ const thoughtData = [
     ],
   },
 ];
-
-// console.log('thoughtData:', thoughtData);
 
 module.exports = { userData, thoughtData };
